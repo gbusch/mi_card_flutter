f4d6d4bdff95d7606389556cc26d46296b20d189
Gerold Busch <gerold.busch@gmail.com>
2019-10-27 17:39:41 +0100

    my version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/penguin.jpg'),
+              ),
+              Text(
+                'Penguin',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontFamily: 'RockSalt',
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+49 1234 567890',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                      fontFamily: 'Ubuntu',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'penguin@antartica.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                      fontFamily: 'Ubuntu',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
